241d380a2b1e36806edf405d6a31c2324009dad0
Timothy Shores <timothymshores@gmail.com>
2019-01-16 22:10:03 -0600

    Added forEach loop, created delete feature using splice() and refactored code into functions

--- list.js
+++ list.js
@@ -6,14 +6,38 @@ window.setTimeout(function() {
     
     while (input != 'quit') {
         if (input == 'list') {
-            console.log(todos);
+            listTodos();
         } else if (input == 'new') {
-            var newTodo = prompt("Add a new to do item");
-            todos.push(newTodo);
-            // console.log(todos);
+            addToDo();
+        } else if (input == 'delete') {
+            deleteToDo();
         }
         input = prompt("What would you like to do?");
     }
     console.log('Quitting the Javascript To Do App');
+    console.log(' ');
+
+
+    function listTodos() {
+        console.log('**********');
+            todos.forEach(function(todo, index){
+                console.log(index +": " + todo);
+            });
+            console.log('***********');
+            console.log(' ');
+    }
+
+    function addToDo() {
+        var newTodo = prompt("Add a new to do item");
+        todos.push(newTodo);
+    }
+
+    function deleteToDo() {
+        var index = prompt("Enter the index of the todo to delete");
+        todos.splice(index, 1);
+        console.log('Item deleted');
+        console.log(' ');
+
+    }
 
 }, 500);
