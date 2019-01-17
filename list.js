43a74d2cc4019271e29dee1737b48c9358781a34
Timothy Shores <timothymshores@gmail.com>
2019-01-16 21:21:11 -0600

    Finished creating basic Javascript to do app

--- list.js
+++ list.js
@@ -1,1 +1,19 @@
-console.log('Hello World!');
+// Avoid 
+window.setTimeout(function() {
+    var todos = ['Add more to do items'];
+    
+    var input = prompt("What would you like to do?");
+    
+    while (input != 'quit') {
+        if (input == 'list') {
+            console.log(todos);
+        } else if (input == 'new') {
+            var newTodo = prompt("Add a new to do item");
+            todos.push(newTodo);
+            // console.log(todos);
+        }
+        input = prompt("What would you like to do?");
+    }
+    console.log('Quitting the Javascript To Do App');
+
+}, 500);
